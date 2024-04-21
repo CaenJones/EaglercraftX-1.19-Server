5cc6230f77b52b995f8fbcf91da94dfcccfbbedd
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-21 12:11:29 -0400

    Update bungee.sh

--- bungee/bungee.sh
+++ bungee/bungee.sh
@@ -1,10 +1,10 @@
-echo "Starting server..."
+echo "Starting Bungee..."
 java -Dlog4j2.formatMsgNoLookups=true -Xms4G -Xmx4G -jar bungee.jar &
 pid=$!
 ‎
 while true; do
     if ! kill -0 $pid 2>/dev/null; then
-        echo "Detected server in crashed/stopped state!"
+        echo "Detected Bungeecord in crashed/stopped state!"
         echo "Restarting..."
         java -Dlog4j2.formatMsgNoLookups=true -Xms4G -Xmx4G -jar bungee.jar &
         pid=$!

1933bda5d152b6c00d7e97c7795a5d422188b09a
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-21 21:38:49 -0400

    Update install.bat

--- testing/windows/install.bat
+++ testing/windows/install.bat
@@ -1,1 +1,28 @@
+@echo off
+
+:: Assuming git, curl, and JRE/JDK are already installed
+
+:: Clone the repository
+git clone https://github.com/CaenJones/EaglercraftX-1.19-Server
+
+:: Rename the directory
+ren EaglercraftX-1.19-Server server
+
+:: Assuming server.bat and bungee.bat are already present in the server and bungee directories
+
+:: Prompt the user to start the server
+echo Repo Installed! Start Server? (y/n)
+set /p startServer=
+
+if /I "%startServer%"=="y" (
+    echo Starting server...
+    start /B server\server.bat
+    start /B server\bungee\bungee.bat
+) else if /I "%startServer%"=="n" (
+    echo Use server\server.bat to start server!
+    curl -O https://github.com/CaenJones/EaglercraftX-1.19-Server/blob/main/testing/linux/run.sh
+    echo run.sh downloaded. Use it to start the server.
+) else (
+    echo Invalid input. Please enter 'y' for yes or 'n' for no.
+)
 

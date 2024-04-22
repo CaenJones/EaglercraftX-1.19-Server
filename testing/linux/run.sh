551bc1dd7918ff463fc8001b1543d6bfb6178416
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-21 21:36:53 -0400

    Update run.sh

--- testing/linux/run.sh
+++ testing/linux/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Starting server...
 mv EaglercraftX-1.19-Server server
-chmod +x /server/server/server.sh
-chmod +x /server/bungee/bungee.sh
+chmod +x server/server/server.sh
+chmod +x server/bungee/bungee.sh
 ./server/server/server.sh &
 ./server/bungee/bungee.sh &

5046de92bf0a11aec5943ba79a173e73dd69a9f6
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-22 15:47:29 -0400

    Update server.sh

--- server/server.sh
+++ server/server.sh
@@ -1,12 +1,12 @@
 echo "Starting server..."
-java -Dlog4j2.formatMsgNoLookups=true -Xms4G -Xmx4G -jar server.jar &
+java -Dlog4j2.formatMsgNoLookups=true -Xms4G -Xmx4G -jar server/server/server.jar &
 pid=$!
 ‎
 while true; do
     if ! kill -0 $pid 2>/dev/null; then
         echo "Detected server in crashed/stopped state!"
         echo "Restarting..."
-        java -Dlog4j2.formatMsgNoLookups=true -Xms4G -Xmx4G -jar server.jar &
+        java -Dlog4j2.formatMsgNoLookups=true -Xms4G -Xmx4G -jar server/server/server.jar &
         pid=$!
     fi
     sleep 5 

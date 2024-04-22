7af52c5cebf865306260f704293a1b091a17ca41
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-21 21:00:42 -0400

    Create server.bat

---
+++ server/server.bat
@@ -0,0 +1,17 @@
+@echo off
+echo Starting Server...
+start /B java -Dlog4j2.formatMsgNoLookups=true -Xms4G -Xmx4G -jar server.jar
+set pid=%errorlevel%
+
+:loop
+timeout /t 5 /nobreak >nul
+tasklist /FI "PID eq %pid%" 2>NUL | find /I /N "java.exe">NUL
+if "%errorlevel%"=="0" (
+    echo Server is running.
+) else (
+    echo Detected Server in crashed/stopped state!
+    echo Restarting...
+    start /B java -Dlog4j2.formatMsgNoLookups=true -Xms4G -Xmx4G -jar server.jar
+    set pid=%errorlevel%
+)
+goto loop

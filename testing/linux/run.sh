f21da620695835d0242ba8dcf5c9ee20dfa3320f
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-22 15:52:34 -0400

    Delete testing/linux/run.sh

--- testing/linux/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo Starting server...
-mv EaglercraftX-1.19-Server server
-chmod +x server/server/server.sh
-chmod +x server/bungee/bungee.sh
-./server/server/server.sh &
-./server/bungee/bungee.sh &

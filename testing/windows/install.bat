06b8d7ec1fe4273a426116a33c936ed7d32bf028
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-22 07:21:21 -0400

    remove git dependency

--- testing/windows/install.bat
+++ testing/windows/install.bat
@@ -1,6 +1,9 @@
 @echo off
-git clone https://github.com/CaenJones/EaglercraftX-1.19-Server
-ren EaglercraftX-1.19-Server server
+echo Downloading EaglercraftX-1.19-Server...
+curl -L -o EaglercraftX-1.19-Server.zip https://github.com/CaenJones/EaglercraftX-1.19-Server/archive/refs/heads/main.zip
+echo Extracting files...
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('EaglercraftX-1.19-Server.zip', '.'); }"
+ren EaglercraftX-1.19-Server-main server
 echo Repo Installed! Start Server? (y/n)
 set /p startServer=
 
@@ -10,9 +13,7 @@ if /I "%startServer%"=="y" (
     start /B server\bungee\bungee.bat
 ) else if /I "%startServer%"=="n" (
     echo Use server\server.bat to start server!
-    curl -O https://github.com/CaenJones/EaglercraftX-1.19-Server/blob/main/testing/linux/run.sh
-    echo run.sh downloaded. Use it to start the server.
+    echo run.sh is not applicable on Windows. Please use server\server.bat to start the server.
 ) else (
     echo Invalid input. Please enter 'y' for yes or 'n' for no.
 )
-

59404dad0fea33d31243ab539cca40ce190611b4
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-22 15:53:48 -0400

    Update install.bat

--- testing/windows/install.bat
+++ testing/windows/install.bat
@@ -1,21 +1,8 @@
 @echo off
-cd C:\%USERPROFILE%\Downloads
 echo Downloading EaglercraftX-1.19-Server...
-curl -O https://github.com/CaenJones/EaglercraftX-1.19-Server/blob/main/testing/windows/run.bat
 curl -L -o EaglercraftX-1.19-Server.zip https://github.com/CaenJones/EaglercraftX-1.19-Server/archive/refs/heads/main.zip
 echo Extracting files...
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('EaglercraftX-1.19-Server.zip', '.'); }"
 ren EaglercraftX-1.19-Server-main server
-echo Repo Installed! Start Server? (y/n)
-set /p startServer=
+echo Server installed! Please read documentation before starting...
 
-if /I "%startServer%"=="y" (
-    echo Starting server...
-    start /B server\server\server.bat
-    start /B server\bungee\bungee.bat
-) else if /I "%startServer%"=="n" (
-    echo Use run.bat in your downloads folder to start the server!
-    echo run.sh is not applicable on Windows. Please use server\server.bat to start the server.
-) else (
-    echo Invalid input. Please enter 'y' for yes or 'n' for no.
-)

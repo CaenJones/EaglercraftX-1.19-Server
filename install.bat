c0cb3a0f72e9adf5d494719d5539ec9df7ece488
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-24 17:13:05 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,4 +6,5 @@ powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compressi
 ren EaglercraftX-1.19-Server-main server
 echo Server installed! Please read documentation before starting...
 echo https://github.com/CaenJones/EaglercraftX-1.19-Server/tree/main#eaglercraftx-119-server
+pause
 

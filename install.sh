f406089bd58a1e0a23d220583f0e827dd93be442
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-24 17:14:05 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ mv EaglercraftX-1.19-Server server
 
 echo Server installed! Please read the documentation before starting...
 echo https://github.com/CaenJones/EaglercraftX-1.19-Server/tree/main#eaglercraftx-119-server
+read

7e0883e317a3549584d450d02181d27b2f5a13c2
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-04-30 13:36:24 +0000

    deleted:    .replit  	deleted:    LicensePrompt.jar  	deleted:    Paper.sh  	deleted:    PlaceHTTPer.class  	deleted:    base.repl  	deleted:    bindmod.so  	deleted:    buildconf.json  	deleted:    buildconf_template.json  	deleted:    cuberite.sh  	deleted:    eula.txt  	deleted:    gitpod-fixes.sh  	deleted:    main.sh  	deleted:    nPaper.sh  	deleted:    replit.nix  	deleted:    reset.txt  	deleted:    selsrv.sh

--- Paper.sh
+++
@@ -1,5 +0,0 @@
-echo "Switching to Paper 1.8.8..."
-rm /tmp/server.jar
-cp misc/paper-1.8.8.jar /tmp/server.jar
-rm server/plugins/Carbon.jar
-rm server/plugins/Carbon-ProtocolLib.jar
--- gitpod-fixes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# only for gitpod
-sudo apt install tmux
-sudo apt-get install dialog
-sudo apt-get update --fix-missing 
-sudo apt install tmux
-sudo apt-get install dialog

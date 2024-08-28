829d7607df8bd3e7015876754cf103eedf373960
Caen Kole Jones <131218155+CaenJones@users.noreply.github.com>
2024-08-28 14:00:36 -0400

    Fix log4j

--- server/server.sh
+++ server/server.sh
@@ -6,7 +6,7 @@ while true; do
     if ! kill -0 $pid 2>/dev/null; then
         echo "Detected server in crashed/stopped state!"
         echo "Restarting..."
-        java -Xmx4G -jar server.jar
+        java -Dlog4j2.formatMsgNoLookups=true -jar server.jar
         pid=$!
     fi
     sleep 5 

01bf708c96e7c7689a703a9740d756e329c415c6
Caen Kole Jones <131218155+CaenJones@users.noreply.github.com>
2024-08-28 13:59:46 -0400

    Update bungee.bat

--- bungee/bungee.bat
+++ bungee/bungee.bat
@@ -11,7 +11,7 @@ if "%errorlevel%"=="0" (
 ) else (
     echo Detected Bungeecord in crashed/stopped state!
     echo Restarting...
-    start /B java -Dlog4j2.formatMsgNoLookups=true -Xms4G -Xmx4G -jar bungee.jar
+    start /B java -Dlog4j2.formatMsgNoLookups=true -jar bungee.jar
     set pid=%errorlevel%
 )
 goto loop
